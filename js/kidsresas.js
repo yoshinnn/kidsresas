f89b74ae247d4bfce7dce0d9e60629f5fd42ca92
YoshinoriMiyawaki <yoshinori@www.srmt.nitech.ac.jp>
2017-11-27 11:48:07 +0900

    kidsresas.js追記

--- js/kidsresas.js
+++ js/kidsresas.js
@@ -62,12 +62,14 @@ function linkToPopComp() {
     var citycode = document.selbox.city.options[citynum].value;
     var cityname;
     var scope;
+    
     if(citycode == "-"){
 	scope = 3//都道府県全体を選択
     }else{
 	scope = 2;//市町村を選択
     }
     var linkTo = "https://resas.go.jp/population-composition/#/map/"+prefcode+"/"+citycode+"/2015/"+scope+"/9.139551352398794/35.07185405/137.44284295";
+    console.log(linkTo)
     window.open(linkTo,'_blank');
 
 }
