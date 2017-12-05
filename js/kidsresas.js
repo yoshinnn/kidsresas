c1b058989b447d7acbddd7bd45ddeeaded50a72c
YoshinoriMiyawaki <yoshinori@www.srmt.nitech.ac.jp>
2017-12-05 21:11:55 +0900

    将来人口推計表示

--- js/kidsresas.js
+++ js/kidsresas.js
@@ -153,6 +153,50 @@ function linkToPopSum() {
 
 }
 
+//RESASの人口増減率のページに直接リンクする                                                                                                                     
+function linkToPopSum() {
+
+    var prefnum = document.selbox.pref.selectedIndex;
+    var prefcode = document.selbox.pref.options[prefnum].value;
+    var prefname = document.selbox.pref.options[prefnum].innerText;
+    var citynum = document.selbox.city.selectedIndex;
+    var citycode = document.selbox.city.options[citynum].value;
+    var cityname;
+    var scope = 0;
+    if(citycode == "-"){
+        scope = 1;//都道府県全体を選択
+    }else{
+        scope = 2;//市町村を選択                                                                                                                                                                
+    }
+
+    var linkTo = "https://resas.go.jp/population-sum/#/graph/"+prefcode+"/"+citycode+"/2015/"+scope+"/9.139551352398794/35.07185405/137.44284295";
+    console.log(linkTo);
+    window.open(linkTo,'_blank');
+
+}
+
+
+//RESASの人口増減率のページに直接リンクする
+function linkToPopFur() {
+//グラフは市町村単位でのみ表示
+    var prefnum = document.selbox.pref.selectedIndex;
+    var prefcode = document.selbox.pref.options[prefnum].value;
+    var prefname = document.selbox.pref.options[prefnum].innerText;
+    var citynum = document.selbox.city.selectedIndex;
+    var citycode = document.selbox.city.options[citynum].value;
+    var cityname;
+    var scope = 0;
+    if(citycode == "-"){
+        scope = 0;//都道府県全体を選択
+    }else{
+        scope = 0;//市町村を選択
+    }
+
+    var linkTo = "https://resas.go.jp/population-future/#/graph/"+prefcode+"/"+citycode+"/2015/"+scope+"/9.139551352398794/35.07185405/137.44284295";
+    console.log(linkTo);
+    window.open(linkTo,'_blank');
+
+}
 
 //農業部門別販売金額を表示
 function drawAgriChart() {
