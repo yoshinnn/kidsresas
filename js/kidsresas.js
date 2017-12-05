02c42500ec854336b9723299278def953512d91e
YoshinoriMiyawaki <yoshinori@www.srmt.nitech.ac.jp>
2017-12-06 00:05:54 +0900

    観光資源

--- js/kidsresas.js
+++ js/kidsresas.js
@@ -287,7 +287,30 @@ function linkToMuniComp() {
 
 }
 
+//RESASの企業数のページに直接リンクする
+function linkToMuniVal() {
 
+    var prefnum = document.selbox.pref.selectedIndex;
+    var prefcode = document.selbox.pref.options[prefnum].value;
+    var prefname = document.selbox.pref.options[prefnum].innerText;
+    var citynum = document.selbox.city.selectedIndex;
+    var citycode = document.selbox.city.options[citynum].value;
+    var cityname;
+    var scope;
+    if(citycode == "-"){
+        scope = 1;//都道府県全体を選択                                                                                                                                                          
+    }else{
+        scope = 2;//市町村を選択                                                                                                                                                          
+    }
+    var larClass = "-";
+    var midClass = "-";
+    var year = 2015;
+
+    var linkTo = "https://resas.go.jp/municipality-value/#/graph/"+prefcode+"/"+citycode+"/"+year+"/"+larClass+"/"+midClass+"/"+scope+"/9.139551352398794/35.07185405/137.44284295";
+    console.log(linkTo);
+    window.open(linkTo,'_blank');
+
+}
 
 //農業部門別販売金額を表示
 function drawAgriChart() {
