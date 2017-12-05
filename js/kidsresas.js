fb17212d1223ebc1dfc1c138b6dd8e5eea8a43a3
YoshinoriMiyawaki <yoshinori@www.srmt.nitech.ac.jp>
2017-12-05 23:18:16 +0900

    観光資源j

--- js/kidsresas.js
+++ js/kidsresas.js
@@ -230,6 +230,36 @@ function linkToTourFromVisitor() {
 }
 
 
+//RESASの観光資源（目的地）のグラフに直接リンクする                                                                                                                                                                       
+function linkToTourDest() {
+
+    var prefnum = document.selbox.pref.selectedIndex;
+    var prefcode = document.selbox.pref.options[prefnum].value;
+    var prefname = document.selbox.pref.options[prefnum].innerText;
+    var citynum = document.selbox.city.selectedIndex;
+    var citycode = document.selbox.city.options[citynum].value;
+    var cityname;
+    var scope = 0;
+    if(citycode == "-"){
+        scope = 1;//都道府県全体を選択                                                                                                                                      
+    }else{
+        scope = 2;//市町村を選択                                                                                                                                            
+    }
+    var scope = 0;
+    var year = 2015;
+    var month = "-";//年度:- 月:1,2,3,...
+    var region = "-";
+    var country = "-";
+    var way = "1";//自動車:1, 公共交通:2
+    var date = "1";//平日:1, 休日:2
+// 11/11100/1/0(スコープ)/2015/-(元の散布図の時期)/2(平日休日)/2(交通手段)/2015/-(表示年月)
+// 1/01100/2/0/2014/-/1/1/2014/-
+    
+    var linkTo = "https://resas.go.jp/tourism-destination/#/toList/9.139551352398794/35.07185405/137.44284295/"+prefcode+"/"+citycode+"/2/"+scope+"/"+year+"/"+month+"/"+date+"/"+way+"/"+year+"/"+month;
+    console.log(linkTo);
+    window.open(linkTo,'_blank');
+
+}
 
 //農業部門別販売金額を表示
 function drawAgriChart() {
